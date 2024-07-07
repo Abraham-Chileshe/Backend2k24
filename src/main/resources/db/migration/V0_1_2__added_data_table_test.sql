3ca9b9d6ae2a616c4d0da680ce38fe093d04decd
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2024-07-07 19:37:47 +1000

    New updates

--- src/main/resources/db/migration/V0_1_2__added_data_table_test.sql
+++ src/main/resources/db/migration/V0_1_2__added_data_table_test.sql
@@ -1,1 +1,2 @@
-INSERT INTO "user"  ("username", "password") VALUES ('Chileshe', 12345678)
+INSERT INTO "user"  ("username", "password") VALUES ('Chileshe', '12345678');
+INSERT INTO "user"  ("username", "password") VALUES ('Abraham', '00110011');
